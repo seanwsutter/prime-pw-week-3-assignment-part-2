0df1fef272f4d796fae33b9a087e33b7a046b71f
seanwsutter <74794351+seanwsutter@users.noreply.github.com>
2021-05-21 00:30:42 -0500

    finished 1a, 1b, 1c

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -1,5 +1,4 @@
 console.log('****** Loops Practice *******');
-
 // 1. 'for' loop
 console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
@@ -26,14 +25,15 @@ for (let i=3; i<6; i++) {
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-let evenNumbers = [2, 4, 6, 8, 10]
-for (let i=0; i<11; i++) {
-  console.log(evenNumbers);
+for (let i=2; i<11; i+=2) {
+  console.log(i);
 }
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (let i=5; i>=0; i--) {
+  console.log(i);
+}
 
 // 2. For of loops
 console.log('---- 2. For Of loop ----');
